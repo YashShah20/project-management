c11f0cfdaf7be151de029bddfc78d0964b95efd8
Yash Shah <yshah@argusoft.com>
2023-07-22 18:15:44 +0530

    project api changes

--- server/routes/project.js
+++ server/routes/project.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { auth } = require("../middlewares/auth");
+const { auth, projectLeadAuth } = require("../middlewares/auth");
 const {
   getProjects,
   getProjectById,
@@ -8,6 +8,7 @@ const {
   updateProjectStatus,
   getProjectTitles,
   getTasksByProjectId,
+  addProjectUsers,
 } = require("../controllers/project");
 const router = express.Router();
 
@@ -15,8 +16,14 @@ router.get("/", auth, getProjects);
 router.get("/titles", auth, getProjectTitles);
 router.get("/:project_id/tasks", auth, getTasksByProjectId);
 router.get("/:project_id(\\d+)", auth, getProjectById);
-router.post("/add", auth, addProject);
-router.put("/:project_id(\\d+)/update", auth, updateProject);
-router.put("/:project_id(\\d+)/update-status", auth, updateProjectStatus);
+router.post("/add", projectLeadAuth, addProject);
+router.put("/:project_id(\\d+)/update", projectLeadAuth, updateProject);
+router.put(
+  "/:project_id(\\d+)/update-status",
+  projectLeadAuth,
+  updateProjectStatus
+);
+
+router.post("/:project_id(\\d+)/add-users", projectLeadAuth, addProjectUsers);
 
 module.exports = router;
